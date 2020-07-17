e7a224b79107006ff878595bcc450dad53579872
pairohit16 <pairohit16@gmail.com>
2020-07-17 10:19:03 +0530

    local time format fixed

--- src/time/local.ts
+++ src/time/local.ts
@@ -1,5 +1,5 @@
 import moment from "moment-timezone";
 
 export function UTC2Local(UTC: string) {
-  return moment.utc(UTC).format("DD MMM, YYYY");
+  return moment.utc(UTC).format("MMM DD, YYYY");
 }
