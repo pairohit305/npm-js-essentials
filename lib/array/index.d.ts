4928541cbbb08a959474ec60fe8fe3cdd98fbd27
pairohit16 <pairohit16@gmail.com>
2020-07-29 19:24:20 +0530

    typescipt types fixed

---
+++ lib/array/index.d.ts
@@ -0,0 +1,2 @@
+export declare function splitArrayInto(array: any[], into: number): any[][];
+//# sourceMappingURL=index.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,4 +7,5 @@ export * from "./uid";
 export * from "./email";
 export * from "./text";
 export * from "./parser";
+export * from "./array";
 //# sourceMappingURL=index.d.ts.map
